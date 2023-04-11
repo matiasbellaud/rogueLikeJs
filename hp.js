a7e3a83dbb3b2f92d9dcaeb5c3ffffb32ddf04a5
belenus <bellaud.matias@gmail.com>
2023-04-11 13:04:45 +0200

    clean code + map

---
+++ hp.js
@@ -0,0 +1,30 @@
+let canvas = document.querySelector('#char');
+let ctx = canvas.getContext('2d');
+
+export default class Hp {
+    constructor(){
+      this.currentHp = 3
+      this.maxHp = 5
+    }
+  
+    takeDamage(){
+      if (this.currentHp > 0){
+        this.currentHp--
+      } 
+    }
+  
+    draw(){
+      let fullHeartImage = new Image(100, 200);
+      fullHeartImage.src = './assets/fullHeart.png';
+      let emptyHeartImage = new Image(100, 200);
+      emptyHeartImage.src = './assets/emptyHeart.png';
+      for (let i=1;i<=this.maxHp;i++){ 
+        if (i<=this.currentHp){
+          ctx.drawImage(fullHeartImage, (16+8)*i-8,16,16,16);
+        } else {
+          ctx.drawImage(emptyHeartImage, (16+8)*i-8,16,16,16);
+        }
+        
+      }
+    }
+  }
